cdd15fec8f7838d943ec38e7df608d00f4544d58
DiegoCS77 <117437024+DiegoCS77@users.noreply.github.com>
2024-01-11 11:42:44 +0100

    Gitignore incluido.

--- credentials.py
+++ credentials.py
@@ -1,12 +1,17 @@
 from dotenv import load_dotenv
-
+import os
 
 class Credentials:
-    def __init__(self, OPENAI, DALLE, LEONARDO):
-        self.OPENAI = OPENAI
-        self.DALLE = DALLE
-        self.LEONARDO = LEONARDO
-
+    
     def load_keys(self):
         load_dotenv()
+    
+    def __init__(self, OPENAI, DALLE, LEONARDO):
+        self.load_keys()
+        
+        self.OPENAI = os.getenv("OPENAI")
+        self.DALLE = os.getenv("DALLE")
+        self.LEONARDO = os.getenv("LEONARDO")
+
+
 
