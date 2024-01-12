8524ba8d9a9214e5718a1c030eb533b3518e7f49
DiegoCS77 <117437024+DiegoCS77@users.noreply.github.com>
2024-01-12 19:52:26 +0100

    Importación sin comentar en el main.
    Comenté audio_analysis para hacer unas pruebas.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-#import audio_analysis as an
-from credentials import Credentials
+import audio_analysis as an
+#from credentials import Credentials
 
-'''nombre = an.record_audio()
+nombre = an.record_audio()
 print(an.speech_to_text(nombre))
-an.delete_audio(nombre)'''
+an.delete_audio(nombre)
 
-print(Credentials.OPENAI)
