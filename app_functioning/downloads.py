e6f6521ba7be09264ce918a185f4d97d8aabdc2e
javipani12 <javier.fernandez.paniagua.22@gmail.com>
2024-02-14 14:02:38 +0100

    Deleting dead code

--- app_functioning/downloads.py
+++ app_functioning/downloads.py
@@ -4,27 +4,18 @@ from datetime import datetime
 import requests
 
 
-def download_image(url, ai, variation=False, name=""):
+def download_image(url, ai):
     """
     This method is used to download an image using its URL
 
     :param url: (String) URL of the image to download
     :param ai: (String) This param indicates which AI has generated the photo
-    :param variation: (Boolean) It's used to determinate if the photo of the URL
-    is a new one or a variation of other one
-    :param name: (String) If variation is True, then this variable will be used to create
-    the name of the image based on name of the original photo
     :return: (String) The file name of the downloaded image
     """
     now = datetime.now()
     data = requests.get(url).content
 
-    if variation:
-        # If it's a variation image from the original, we get the name of it
-        file_name = name.split(".")[0] + "_variation." + name.split(".")[1]
-    else:
-        # If it's a new image, we create a name for it
-        file_name = f"media/images/{ai}_" + now.strftime("%d%m%y_%H%M%S") + ".png"
+    file_name = f"media/images/{ai}_" + now.strftime("%d%m%y_%H%M%S") + ".png"
 
     file = open(file_name, "wb")
     file.write(data)
